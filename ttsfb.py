f3b1064d094f8a0bbf53575daedec588405481a2
SterverSVK <sterver12345@gmail.com>
2014-12-27 17:47:48 +0100

    Translated to English

--- ttsfb.py
+++ ttsfb.py
@@ -26,7 +26,7 @@ import threading
 from os import system as x
 startupinfo = None
 #import logging    
-#logging.basicConfig(level=logging.ERROR)
+#logging.basicConfig(level=logging.DEBUG)
 
 sys.path.append("./lib")
 from sleekxmpp import ClientXMPP
@@ -44,9 +44,7 @@ if os.name == "nt":
         if (os.path.isfile(road)):
             pass
         else:
-            print ("Aplikácia pre prehrávanie zvuku potrebuje VLC Media \
-Player. V základných umiestneniah sa nenachádza. Správy \
-nebudú prečítané nahlas.")
+            print ("Application require VLC Media Player. Messages will not be readed.")
     
     def player():
         return "\"" + road + "\" --play-and-exit --intf dummy "
@@ -64,15 +62,15 @@ else:
                     return "vlc --play-and-exit --intf dummy "
                 break
             else:
-                print ("Pre používanie je potrebné mať nainštalovaný \
-mplayer alebo VLC media player.\n\
-Prosím, doinštalujte ho a pokračujte stlačením klávesy Enter.")
+                print ("Application require: \n\
+mplayer or VLC media player.\n\
+Please install it and continue by press Enter.")
                 input ()
 
 
 #### APP
 class Settings:
-    lang = "sk"
+    lang = "en"
     names = []
     read_name = True
     whitelist = True
@@ -115,14 +113,13 @@ def cls():
 def session_start(event):
     fbtts.send_presence()
     Loading.stop()
-    print("Pripojené..")
+    print("Connected..")
     fbtts.get_roster()
 
 
 def startup_q():
-    print ("Prajete si spustiť program so základnými nastaveniami?\n\
-(v slovenskom jazyku číta správy od všetkých používateľov.\n\
-Ku menu sa dá počas spojenia prisúpiť zadaním príkazu menu)")
+    print ("Do you want to launch application with default settings?\n\
+(You can access menu by typing menu while application is running)")
     volba = input ("Y/n: ")
     if volba in ["Y", "y", '']:
         return
@@ -134,50 +131,49 @@ def menu():
     cls()
     
     if len(Settings.names) == 0:
-        i_names = "všetci"
+        i_names = "all"
     else:
         i_names = len(Settings.names)
     
     print ("""Menu:
-1 -> Nastavenie jazyka (nastavený: {0})
-2 -> Nastavenie zoznamu sledovaných užívateľov (nastavených: {1})
-3 -> Prečítanie mena pred správou: {2}
+1 -> Set language (set: {0})
+2 -> Userlist settings (count: {1})
+3 -> Read name: {2}
 
-0 -> Spustiť
+0 -> Run
 
 q -> exit()
 """.format(Settings.lang, i_names, Settings.read_name))
     
-    volba = input("Zdajte číslo: ")
+    choice = input("Enter your choice: ")
     
-    if volba == "":
+    if choice == "":
         cls()
         return
-    elif volba in ("q", "exit"):
+    elif choice in ("q", "exit"):
         os._exit(1)
     try:
-        volba = int(volba)
+        choice = int(volba)
     except:
         cls()
-        print("Chyba! Nezadali ste číslo (Integer).")
+        print("Error! Not a valid choice.")
         menu()
         return
     
-    if volba == 0:
+    if choice == 0:
         return
-    if volba == 1:
-        Settings.lang = input("Zadajte kódové označenie jazyka \
-(napr. sk, en, de): ")
+    if choice == 1:
+        Settings.lang = input("Type language code (for example. sk, en, de): ")
         cls()
-    elif volba == 2:
+    elif choice == 2:
         cls()
         set_users()
-    elif volba == 3:
+    elif choice == 3:
         Settings.read_name = not Settings.read_name
         cls()
     else:
         cls()
-        print("Chyba! Zadali ste nesprávne číslo.")
+        print("Error! You have entered wrong number.")
 
     menu()
 
@@ -185,22 +181,22 @@ q -> exit()
 def set_users():
     cls()
     if len(Settings.names) == 0:
-        i_names = "všetci"
+        i_names = "all"
     else:
         i_names = Settings.names
     if (Settings.whitelist):
         i_wlist = "[WHITELIST] /  BLACKLIST "
-        i_wlstw = "sledovaní"
+        i_wlstw = "read"
     else:
         i_wlist = " WHITELIST  / [BLACKLIST]"
-        i_wlstw = "nesledovaní"
+        i_wlstw = "don’t read"
     
-    print ("!> Momentálne {0} užívatelia:\n    {1}".format(i_wlstw, i_names))
-    print ("""!> Dostupné príkazy: 
-    add [užívateľ] (pridá užívateľa) 
-    del [užívateľ] (zmaže užívateľa)
-    clear          (vyčistí zoznam)
-    wb (zmení prístup k užívateľom v zozname) """ + i_wlist)
+    print ("!> Currently {0} users:\n    {1}".format(i_wlstw, i_names))
+    print ("""!> Available commands: 
+    add [user] 
+    del [user]
+    clear          (clear list)
+    wb (change access to users) """ + i_wlist)
     
     while 1:
         command = input("?> ")
@@ -211,7 +207,7 @@ def set_users():
         
         if command == "add":
             if name in Settings.names: 
-                print("!> Meno sa už v zozname nachádza.")
+                print("!> The user is already in list")
             else:
                 Settings.names.append(name)
                 break
@@ -220,7 +216,7 @@ def set_users():
                 del Settings.names [Settings.names.index(name)]
                 break
             else:
-                print ("!> Meno sa v zozname nenachádza.")
+                print ("!> This user not on the list.")
         elif command == "clear":
             Settings.names = []
             break
@@ -235,13 +231,13 @@ def set_users():
             cls()
             return
         else:
-            print ("!> Chybný príkaz!")
+            print ("!> Wrond command!")
 
     set_users()
 
 
-def id_to_name(idecko):
-    url = "http://graph.facebook.com/" + idecko
+def id_to_name(iddie):
+    url = "http://graph.facebook.com/" + iddie
     raw_data = urllib.request.urlopen(url).read()
     data = json.loads(raw_data.decode("utf8"))
     name = data["name"]
@@ -250,10 +246,10 @@ def id_to_name(idecko):
 
 def read(name, text):
     if Settings.read_name:
-        toread = name + " píše: " + text
+        toread = name + " wrote: " + text
     else:
         toread = text
-    print ("Čítam: " + name + " píše: " + text)
+    print ("Reading: " + name + " wrote: " + text)
     uurrll = "\"http://translate.google.com/translate_tts?tl="+Settings.lang+"\
 &q="+toread+"&ie=UTF8\""
     if os.name=="nt":
@@ -276,17 +272,17 @@ def message(msg):
             if (name in Settings.names) == Settings.whitelist:
                 read (name, rmsg)
             else:
-                print(name + " píše: " + rmsg)
+                print(name + " wrote: " + rmsg)
 
 
-def kvit():
+def kwit():
     inpul = input()
     if inpul == "exit":
         print ("Exitting...")
         os._exit(1)
     elif inpul == "menu":
         menu()
-    kvit()
+    kwit()
 
 
 def check_username(usrn):
@@ -310,7 +306,7 @@ def check_username(usrn):
 def get_info():
     jid = input("Username: ")
     while not check_username(jid):
-        print ("Zadajte správne prihlasovacie meno.")
+        print ("Type correct login name.")
         jid = input("Username: ")
     jid += "@chat.facebook.com"
     password = getpass.getpass("Password: ")
@@ -323,9 +319,9 @@ def main():
     jid,password = get_info()
     server = ("chat.facebook.com", 5222)
 
-    print ("Pripájanie... (môže trvať niekľko minút)")
+    print ("Connecting... (it may takes some minutes)")
     Loading.start()
-    threading.Thread(target=kvit).start()
+    threading.Thread(target=kwit).start()
     
     global fbtts
     fbtts = ClientXMPP(jid,password)
