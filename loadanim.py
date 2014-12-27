bd1288ab912bf365f058792d39740cebf065f1ae
Tomáš Vician <tomas.vician@st.fri.uniza.sk>
2014-12-27 21:36:43 +0100

    animation

---
+++ loadanim.py
@@ -0,0 +1,52 @@
+def genAnim():
+    moves=[
+"""
+   
+|"|
+""",
+"""
+   
+/"|
+""",
+"""
+_  
+ "|
+""",
+"""
+\  
+ "|
+""",
+"""
+ | 
+ ? 
+""",
+"""
+  /
+|" 
+""",
+"""
+  _
+|" 
+""",
+"""
+   
+|"\\
+"""
+]
+    m=0
+    result=[]
+    for i in range(0,41):
+        spaceFirst= ""
+        spaceLast= ""
+        
+        for j in range(0, i):
+            spaceFirst+= " "
+        for j in range(0, 41-i):
+            spaceLast+=  " "
+        result += [moves[m]]
+        result[i]=result[i].replace("\n","\n"+spaceFirst)
+        result[i]=result[i].replace("\n",spaceLast+"\n")
+        m=m+1
+        if m>7:
+            m=0
+    return result+result[::-1]
